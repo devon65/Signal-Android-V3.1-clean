73c920d9041c9d692a1056f86ccfcd718d7e16ba
devon65 <how.d.65@gmail.com>
2018-07-12 15:05:00 -0600

    may be adding unneccisery files heare!

--- build/generated/source/buildConfig/play/debug/org/thoughtcrime/securesms/BuildConfig.java
+++ build/generated/source/buildConfig/play/debug/org/thoughtcrime/securesms/BuildConfig.java
@@ -14,7 +14,7 @@ public final class BuildConfig {
   public static final String NOPLAY_UPDATE_URL = null;
   public static final boolean PLAY_STORE_DISABLED = false;
   // Fields from default config.
-  public static final long BUILD_TIMESTAMP = 1531426373000L;
+  public static final long BUILD_TIMESTAMP = 1531428359000L;
   public static final boolean DEV_BUILD = false;
   public static final String GIPHY_PROXY_HOST = "giphy-proxy-production.whispersystems.org";
   public static final int GIPHY_PROXY_PORT = 80;
